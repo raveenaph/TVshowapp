396e6f2241e44b8e5478ed29eb967f61c5dd6578
raveenaph <114966154+raveenaph@users.noreply.github.com>
2024-12-30 21:14:50 -0700

    main page and search function

---
+++ frontend/script.js
@@ -0,0 +1,47 @@
+const APILINK = 'https://api.themoviedb.org/3/discover/tv?include_adult=false&language=en-US&page=1&sort_by=popularity.desc&api_key=823817cb3e1ee53cbf33cbcf268be89a&page=1';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCHAPI = 'https://api.themoviedb.org/3/search/tv?&include_adult=false&language=en-US&api_key=823817cb3e1ee53cbf33cbcf268be89a&page=1&query=';
+
+const showsList = document.getElementById("shows");
+const form = document.getElementById("form");
+const search = document.getElementById("query");
+const searchbutton = document.getElementById("search-button");
+
+displayShows(APILINK);
+
+function displayOneShow(show) {
+    showsList.innerHTML += `
+        <div class="show">
+            <div class="show-container">
+                <img src="${IMG_PATH}${show.poster_path}" class="thumbnail">
+            </div>
+            <h3 class="show-title">${show.name}</h3>
+        </div>`;
+}
+
+function displayShows(url) {
+    showsList.innerHTML = "";
+
+    fetch(url).then(res => res.json())
+    .then(function(data) {
+        console.log(data.results);
+        data.results.forEach(show => {
+            displayOneShow(show);
+        });
+        
+    }); 
+}
+
+searchbutton.addEventListener("click", (e) => {
+    e.preventDefault(); 
+
+    showsList.innerHTML = '';
+
+    const searchItem = search.value;
+
+    if(searchItem) {
+        console.log(SEARCHAPI + searchItem);
+        displayShows(SEARCHAPI + searchItem);
+        search.value = "";
+    }
+});
